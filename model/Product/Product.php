bed6ada9247e25a6a78b98135762752333892485
llambrichd06 <llambrichd06@gmail.com>
2025-11-17 19:01:01 +0100

    EEATS-4 Continuing HTML CSS, did some API tests that were presented to us

---
+++ model/Product/Product.php
@@ -0,0 +1,131 @@
+<?php
+
+class Product
+{
+
+    private $id;
+    private $userId;
+    private $createdAt;
+    private $address;
+    private $deliveryType;
+    private $total;
+    private $subtotal;
+    private $deliveryDate;
+    private $discountId;
+    private $discountApplied;
+
+    public function __construct(
+        $userId = null,
+        $createdAt = null,
+        $address = null,
+        $deliveryType = null,
+        $total = null,
+        $subtotal = null,
+        $deliveryDate = null,
+        $discountId = null,
+        $discountApplied = null,
+        $id = null
+    ) {
+        $this->userId = $userId;
+        $this->createdAt = $createdAt;
+        $this->address = $address;
+        $this->deliveryType = $deliveryType;
+        $this->total = $total;
+        $this->subtotal = $subtotal;
+        $this->deliveryDate = $deliveryDate;
+        $this->discountId = $discountId;
+        $this->discountApplied = $discountApplied;
+        $this->id = $id;
+    }
+
+   public function getDiscountApplied(){
+    return $this->discountApplied;
+}
+
+public function setDiscountApplied($discountApplied){
+    $this->discountApplied = $discountApplied;
+    return $this;
+}
+
+public function getDiscountId(){
+    return $this->discountId;
+}
+
+public function setDiscountId($discountId){
+    $this->discountId = $discountId;
+    return $this;
+}
+
+public function getDeliveryDate(){
+    return $this->deliveryDate;
+}
+
+public function setDeliveryDate($deliveryDate){
+    $this->deliveryDate = $deliveryDate;
+    return $this;
+}
+
+public function getSubtotal(){
+    return $this->subtotal;
+}
+
+public function setSubtotal($subtotal){
+    $this->subtotal = $subtotal;
+    return $this;
+}
+
+public function getTotal(){
+    return $this->total;
+}
+
+public function setTotal($total){
+    $this->total = $total;
+    return $this;
+}
+
+public function getDeliveryType(){
+    return $this->deliveryType;
+}
+
+public function setDeliveryType($deliveryType){
+    $this->deliveryType = $deliveryType;
+    return $this;
+}
+
+public function getAddress(){
+    return $this->address;
+}
+
+public function setAddress($address){
+    $this->address = $address;
+    return $this;
+}
+
+public function getCreatedAt(){
+    return $this->createdAt;
+}
+
+public function setCreatedAt($createdAt){
+    $this->createdAt = $createdAt;
+    return $this;
+}
+
+public function getUserId(){
+    return $this->userId;
+}
+
+public function setUserId($userId){
+    $this->userId = $userId;
+    return $this;
+}
+
+public function getId(){
+    return $this->id;
+}
+
+public function setId($id){
+    $this->id = $id;
+    return $this;
+}
+
+}
--- view/main.php
+++ view/main.php
@@ -4,30 +4,74 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="main.css">
+    integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="../resources/css/main.css">
     <title>Document</title>
 </head>
-<body>
-    <header class="container-fluid text-center">
+<body class="">
+    <header class="container-fluid text-center marge">
         <nav class="row d-flex">
             <div class="col-3">
-                <img src="resources/images/EENameLogo.png" alt="" class="img-fluid">
+                <img src="resources/images/EENameLogo.png" alt="Logo and name of website, Electronic Eats" class="img-fluid">
             </div>
-            <div class="col-6">
-                <ul>
-                    <li>FOODS <div><img src="resources/images/Menu Arrow.png" alt="" class="img-fluid"></div></li>
-                    <li>DISCOUNTS <img src="resources/images/Menu Arrow.png" alt="" class="img-fluid"></li>
+            <div class="col-6 d-flex align-items-center">
+                <ul class="d-flex align-items-center gap-2">
+                    <li><a href="" class="fs-6">FOODS</a></li>
+                    <li><a href="">DISCOUNTS</a></li>
                 </ul>
             </div>
-            <div class="col-3">
-                <img src="resources/images/profileicon.svg" alt="">
-                <img src="" alt="">
+            <div class="col-3 d-flex align-items-center justify-content-end gap-2">
+                <img src="resources/images/profileicon.svg" alt="Profile Icon" class="img-fluid">
+                <img src="resources/images/cartIcon.svg" alt="Cart Icon" class="img-fluid">
             </div>
         </nav>
     </header>
-    <main>
+    <main class="container-fluid text-center marge">
         <?php //include_once $view?>
     </main>
+    <footer class="container-fluid marge">
+        <div class="row-2 d-flex justify-content-between">
+            <nav>
+                <ul class="d-flex align-items-center gap-2">
+                    <li><a href="?controller=Home&action=index&">Home</a></li>
+                    <li><a href="">Foods</a></li>
+                    <li><a href="">Cart</a></li>
+                </ul>
+            </nav>
+            <div class="d-flex flex-column align-items-end">
+                <strong class="text-right">Join The Conversation</strong>
+                <div>
+                    <img src="" alt="Facebook">
+                    <img src="" alt="Twitter">
+                    <img src="" alt="Youtube">
+                    <img src="" alt="Instagram">
+                    <img src="" alt="Twitch">
+                    <img src="" alt="Linkedin">
+                </div>
+            </div>
+        </div>
+        <div class="row-2 d-flex justify-content-between">
+            <div class="d-flex">
+                <img src="../resources/images/LogoEEats.png" alt="Logo EEats" class="img-fluid">
+                <div>
+                    <div>
+                        <a href="">Products</a>
+                        <a href="">Premium</a>
+                        <a href="">About</a>
+                        <a href="">Contact</a>
+                    </div>
+                    <div>
+                        <a href="">Privacy Policy & Cookies</a>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p>Regional Pricing</p>
+                <div class="d-flex">
+                    <select name="" id=""></select>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
