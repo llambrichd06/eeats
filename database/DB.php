5c374bbf6e77df95c09963a8e950c20cb99f78b3
llambrichd06 <llambrichd06@gmail.com>
2026-01-08 17:31:33 +0100

    EEATS-46 Added salad image, changed database connection for docker

--- database/DB.php
+++ database/DB.php
@@ -1,8 +1,8 @@
 <?php
 
 class DB {
-    public static function connect($host='localhost', $user='root', $pass='', $db='eeats') {
-        $con = new mysqli($host, $user, $pass, $db);
+    public static function connect($host='localhost', $user='root', $pass='dev', $db='myeeatsdb', $port=3320) {
+        $con = new mysqli($host, $user, $pass, $db, $port);
 
         if ($con == false) {
             die('Error al conectar a la base de datos');
